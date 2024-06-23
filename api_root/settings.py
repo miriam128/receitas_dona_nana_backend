54451427b16c69ecea7b5b5f110c6d1a2966f700
miriam128 <miriamamaral15@gmail.com>
2024-06-23 17:59:50 -0300

    Atualiza settings para arquivos estáticos

--- api_root/settings.py
+++ api_root/settings.py
@@ -146,9 +146,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-  'http://localhost:5173'
+  'http://localhost:5173',
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
